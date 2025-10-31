322dda987a50f2cdd12f0f228f631701d90a54c0
henriqueapc16 <henriqueapc1671@outlook.com>
2025-10-31 13:45:55 -0300

    NK

---
+++ js copy.js
@@ -0,0 +1,50 @@
+let numero = 4;
+if (numero ==4); {
+
+  console.log( "ok");
+}
+
+let n = 4;
+let nu = "4";
+let soma = n+nu;
+
+console.log (soma);
+
+/*
+let idade = Number(prompt("Qual é a tua idade?"));
+console.log(`Daqui a 5 anos terás ${idade + 5} anos.`);*/
+let nub = 10 
+ nub = nub +1
+console.log( nub)
+let contador = 10
+contador += 5
+
+console.log(contador );
+{
+let balance = 500;
+let pay = 200;
+if (balance >= pay){
+  
+
+console.log ( "tem saldo");}
+else {
+  
+console.log ("sem saldo suficiente ");}
+}
+
+let value 
+value =0
+value  += 7
+value -= 2
+value %=2 
+  console.log( value)
+if (value ===0 ){
+  console.log("o numero é impar")
+}
+
+
+let email = true
+let password= true
+  if ( email && password ){
+  console.log(" acesso liberado")
+  }
